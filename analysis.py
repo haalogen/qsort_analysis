cae27ef57f998a87648579f8a212fb8eb56842ad
stan <citrus_82@nextmail.ru>
2015-10-10 15:17:21 +0300

    Added statistics analysis.

--- analysis.py
+++ analysis.py
@@ -9,11 +9,13 @@ def main():
     N = 6
     M = 6
     TIMES = 10
+    ROWS_NUM = 2 * N + 1
 
     if len(sys.argv) >= 4:
         N = int(sys.argv[1])
         M = int(sys.argv[2])
         TIMES = int(sys.argv[3])
+        ROWS_NUM = 2 * N + 1
     else:
         print """
 Usage: 
@@ -27,71 +29,86 @@ Ex:
     time_qselect = 0
     
     
-    data_matrix = np.zeros( (2 * N + 1, N) )
-
-    fu.init_data_matrix(data_matrix)
-    
-#    print "Initial stand:"
-#    print data_matrix[   :N], '\n'
-#    print data_matrix[N:N+1], '\n'
-#    print data_matrix[N+1: ], '\n'
-
-    rows_num = data_matrix.shape[0] # == 2N+1
-    
-    
-    cmp_simple = np.zeros(rows_num, dtype=int)
-    swp_simple = np.zeros(rows_num, dtype=int)
-    cmp_select = np.zeros(rows_num, dtype=int)
-    swp_select = np.zeros(rows_num, dtype=int)
-    
-    data_matrix2 = copy.deepcopy(data_matrix)
-    sorted_array = data_matrix[0]
-    
-    
-    start_qsort = time.time()
-    for k in range(rows_num):
-        fu.qsort(data_matrix[k])
-        cmp_simple[k] = fu.glob_cmp_cnt
-        swp_simple[k] = fu.glob_swp_cnt
-    
-    finish_qsort = time.time()
-    time_qsort += finish_qsort - start_qsort
-    
-    
-    qsort_is_correct = [False] * rows_num
-    for k in range(rows_num):
-        if all(data_matrix[k] == sorted_array):
-            qsort_is_correct[k] = True
-    
-    
-    print "qsort_is_correct: ", all(qsort_is_correct)
+    cmp_simple = np.zeros(ROWS_NUM)
+    swp_simple = np.zeros(ROWS_NUM)
+    cmp_select = np.zeros(ROWS_NUM)
+    swp_select = np.zeros(ROWS_NUM)
         
     
-    start_qselect = time.time()
-    for k in range(rows_num):
-        fu.qsort_select(data_matrix2[k], M)
-        cmp_select[k] = fu.glob_cmp_cnt
-        swp_select[k] = fu.glob_swp_cnt
+    for tm in range(TIMES):
+        print "Iteration # %r." % tm
+        data_matrix = np.zeros( (ROWS_NUM, N) )
+        
+        fu.init_data_matrix(data_matrix)
+        
+    #    print "Initial stand:"
+    #    print data_matrix[   :N], '\n'
+    #    print data_matrix[N:N+1], '\n'
+    #    print data_matrix[N+1: ], '\n'
+        
+        
+        data_matrix2 = copy.deepcopy(data_matrix)
+        sorted_array = data_matrix[0]
+        
+        
+        start_qsort = time.time()
+        for k in range(ROWS_NUM):
+            fu.qsort(data_matrix[k])
+            cmp_simple[k] += fu.glob_cmp_cnt
+            swp_simple[k] += fu.glob_swp_cnt
+        
+        finish_qsort = time.time()
+        time_qsort += finish_qsort - start_qsort
+        
+        
+        qsort_is_correct = [False] * ROWS_NUM
+        for k in range(ROWS_NUM):
+            if all(data_matrix[k] == sorted_array):
+                qsort_is_correct[k] = True
+        
+        
+        print "qsort_is_correct: ", all(qsort_is_correct)
+            
+        
+        start_qselect = time.time()
+        for k in range(ROWS_NUM):
+            fu.qsort_select(data_matrix2[k], M)
+            cmp_select[k] += fu.glob_cmp_cnt
+            swp_select[k] += fu.glob_swp_cnt
+        
+        finish_qselect = time.time()
+        time_qselect += finish_qselect - start_qselect
+        
+        
+        qsort_select_is_correct = [False] * ROWS_NUM
+        for k in range(ROWS_NUM):
+            if all(data_matrix2[k] == sorted_array):
+                qsort_select_is_correct[k] = True
+        
+        
+        print "qsort_select_is_correct: ", all(qsort_select_is_correct)
+        
+#        print "cmp_simple:", cmp_simple
+#        print "swp_simple:", swp_simple
+#        print "cmp_select:", cmp_select
+#        print "swp_select:", swp_select
+#        print "\n"
     
-    finish_qselect = time.time()
-    time_qselect += finish_qselect - start_qselect
-    
-    
-    qsort_select_is_correct = [False] * rows_num
-    for k in range(rows_num):
-        if all(data_matrix2[k] == sorted_array):
-            qsort_select_is_correct[k] = True
-    
-    
-    print "qsort_select_is_correct: ", all(qsort_select_is_correct)
-    
-    print "time_qsort: %r sec" % time_qsort
-    print "time_qselect: %r sec \n" % time_qselect
-    
+    cmp_simple /= TIMES
+    swp_simple /= TIMES
+    cmp_select /= TIMES
+    swp_select /= TIMES
+    time_qsort /= TIMES
+    time_qselect /= TIMES
+    print "\nStatistics (mean values):"
     print "cmp_simple:", cmp_simple
     print "swp_simple:", swp_simple
     print "cmp_select:", cmp_select
     print "swp_select:", swp_select
+    
+    print "time_qsort: %r sec" % time_qsort
+    print "time_qselect: %r sec \n" % time_qselect
+
     
     
 #    print "Sorted (ascending) by qsort() stand 'data_matrix':"
